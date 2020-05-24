6246068db9beaaf6e74e28a1742fa9c0057d6644
Mug <brad.alexander913@gmail.com>
2020-05-24 16:04:28 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+document.body.removeChild(document.querySelector('main'))
+let newHeader = document.createElement('h1');
+newHeader.id = "victory"
+newHeader.innerHTML = "<h1>Brad is the champion</h1>";
+newHeader.className = "victory";
+
+
+document.head.append(newHeader);
