61ee5f6b5cac5cc110f6853dba38af39fdbbbfd9
github username <github email address>
2020-07-11 19:20:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Write your code here!
-document.querySelector("main").remove()
-let element=document.createElement("h1")
-document.body.appendChild(element)
-document.querySelector("h1").id="victory"
-let newHeader=document.querySelector("h1#victory")
+document.querySelector("main").remove();
+let newHeader=document.createElement("h1")
+newHeader.id="victory"
+document.body.appendChild(newHeader)
 newHeader.innerHTML="Caitlin is the champion"
