d3ac7cec1e504cd59a0f960cd1b9834f122d5355
Caitlin Majewski <cmajewski@wesleyan.edu>
2020-12-15 03:59:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code here!
-document.querySelector("main").remove();
+document.querySelector("main#main").remove();
+
 let newHeader=document.createElement("h1")
 newHeader.id="victory"
-document.body.appendChild(newHeader)
 newHeader.innerHTML="Caitlin is the champion"
